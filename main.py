e06c414317a6ee48f02af6b8c5d561bbdf3ac2cb
marcel41 <mcube41xd@gmail.com>
2020-02-20 18:53:46 +0000

    reading file

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
   print ("GOOGLE HASH READING INPUT")
+  userInput = input('example name (e.g: a,b ):')
+
   #uncomment the following two lines to open a file
-  #file1 = open(".txt", "r")
-  #file1.close()
+  file1 = open(userInput + ".txt", "r")
+  for l in file1:
+    print(l)
+  file1.close()
   #close file to avoid leak out
 
 def func1():
